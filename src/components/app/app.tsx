0334ff0750f2e1266d6bd254f44f47668f925c7a
tkornilova <kornilova.tat07@gmail.com>
2026-04-10 23:13:52 +0400

    feat: add main routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,20 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Routes, Route } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { ProtectedRoute } from '../../utils/protected-route';
 import { Preloader } from '@ui';
 
 const App = () => {
@@ -10,10 +22,108 @@ const App = () => {
   const isIngredientsLoading = false;
   const ingredients = [];
   const error = null;
+  let isAuth = true;
 
   return (
     <div className={styles.app}>
       <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+
+        <Route path='/feed'>
+          <Route index element={<Feed />} />
+          <Route
+            path=':number'
+            element={
+              <Modal
+                title='TITLE'
+                onClose={() => console.log('модалка')}
+                children={<OrderInfo />}
+              />
+            }
+          />
+        </Route>
+
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute isAuth={isAuth} onlyUnAuth>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute isAuth={isAuth} onlyUnAuth>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute isAuth={isAuth} onlyUnAuth>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRoute isAuth={isAuth} onlyUnAuth>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path='/profile'>
+          <Route
+            index
+            element={
+              <ProtectedRoute isAuth={isAuth}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders/:number'
+            element={
+              <Modal
+                title='TITLE'
+                onClose={() => console.log('модалка')}
+                children={<OrderInfo />}
+              />
+            }
+          />
+        </Route>
+
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal
+              title='TITLE'
+              onClose={() => console.log('модалка')}
+              children={<IngredientDetails />}
+            />
+          }
+        />
+
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute isAuth={isAuth}>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
       {isIngredientsLoading ? (
         <Preloader />
       ) : error ? (
