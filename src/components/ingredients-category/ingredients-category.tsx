ee3bad9260f7aacb64770a23c328fb0499162441
tkornilova <kornilova.tat07@gmail.com>
2026-04-15 15:57:53 +0400

    feat: add burgerConstructor data

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -2,29 +2,28 @@ import { forwardRef, useMemo } from 'react';
 import { TIngredientsCategoryProps } from './type';
 import { TIngredient } from '@utils-types';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
+import { useSelector } from '../../services/store';
 
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
-  /** TODO: взять переменную из стора */
-  const burgerConstructor = {
-    bun: {
-      _id: ''
-    },
-    ingredients: []
-  };
+  const { constructorItems } = useSelector((store) => store.burgerConstructor);
 
   const ingredientsCounters = useMemo(() => {
-    const { bun, ingredients } = burgerConstructor;
+    const { bun, ingredients: constructorIngredients } = constructorItems;
+
     const counters: { [key: string]: number } = {};
-    ingredients.forEach((ingredient: TIngredient) => {
+
+    constructorIngredients.forEach((ingredient) => {
       if (!counters[ingredient._id]) counters[ingredient._id] = 0;
       counters[ingredient._id]++;
     });
+
     if (bun) counters[bun._id] = 2;
+
     return counters;
-  }, [burgerConstructor]);
+  }, [constructorItems]);
 
   return (
     <IngredientsCategoryUI
