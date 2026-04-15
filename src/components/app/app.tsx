cb43259e715d68754aff2ea0c5fc779350c3fd71
tkornilova <kornilova.tat07@gmail.com>
2026-04-16 00:14:31 +0400

    fix: fix route (/ingredients/:id)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -149,7 +149,19 @@ const App = () => {
               <Route path='orders/:number' element={<OrderInfo />} />
             </Route>
 
-            <Route path='/ingredients/:id' element={<IngredientDetails />} />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <div className={styles.detailPageWrap}>
+                  <p
+                    className={`text text_type_main-large ${styles.detailHeader}`}
+                  >
+                    Детали ингредиента
+                  </p>
+                  <IngredientDetails />
+                </div>
+              }
+            />
 
             <Route path='*' element={<NotFound404 />} />
           </Routes>
